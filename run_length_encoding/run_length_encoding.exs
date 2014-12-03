f84b140f62e7ff2e444406b2ba280ebbbd11d66e
Kabie <kabie3000@gmail.com>
2014-12-03 22:23:00 +0800

    encode

--- run_length_encoding/run_length_encoding.exs
+++ run_length_encoding/run_length_encoding.exs
@@ -5,8 +5,25 @@ defmodule RunLengthEncoding do
   preceded by the number of items in the run.
   """
 
-  def encode(text) do
+  def encode(<<c>> <> rest_text) do
+    _encode(rest_text, [{1, c}])
+  end
 
+  defp _encode(<<c>> <> rest_text, [{n, c} | rest]) do
+    _encode(rest_text, [{n+1, c} | rest])
+  end
+
+  defp _encode(<<c>> <> rest_text, [{_, _c} | _] = rest) do
+    _encode(rest_text, [{1, c} | rest])
+  end
+
+  defp _encode("", encoded) do
+    encoded
+    |> Enum.reverse
+    |> Enum.map(fn {n, c} ->
+      to_string(n) <> <<c>>
+    end)
+    |> Enum.join
   end
 
   def decode(text) do
