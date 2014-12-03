b562db9757d2a33ad8c6a1a02cde3e20ca5f62f5
Kabie <kabie3000@gmail.com>
2014-12-03 21:24:02 +0800

    skeleton

---
+++ fizzbuzz/fizzbuzz.exs
@@ -0,0 +1,13 @@
+
+defmodule FizzBuzz do
+  @moduledoc """
+  Print the numbers from 1 to 100, replacing multiples of 3 with the word
+  Fizz and multiples of 5 with the word Buzz. For numbers that are divisible
+  by 3 and 5, replace the number with the word FizzBuzz.
+  """
+  def buzz(numbers, rules \\ [Fuzz: 3, Buzz: 5]) do
+  end
+
+end
+
+FizzBuzz.buzz(1..100)
