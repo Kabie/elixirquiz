a860779e8fddb19dd7de2c231793c26ff3508c4f
Kabie <kabie3000@gmail.com>
2014-12-03 21:55:25 +0800

    Fizzbuzz done
    recursively

--- fizzbuzz/fizzbuzz.exs
+++ fizzbuzz/fizzbuzz.exs
@@ -1,4 +1,3 @@
-
 defmodule FizzBuzz do
   @moduledoc """
   Print the numbers from 1 to 100, replacing multiples of 3 with the word
@@ -6,8 +5,25 @@ defmodule FizzBuzz do
   by 3 and 5, replace the number with the word FizzBuzz.
   """
   def buzz(numbers, rules \\ [Fuzz: 3, Buzz: 5]) do
+    _buzz({"", rules}, Enum.to_list(numbers), rules)
+  end
+
+  defp _buzz({s, rules}, [n | rest_numbers], [{bz, m} | rest_rules]) do
+    if rem(n, m) == 0 do
+      _buzz({s <> to_string(bz), rules}, [n | rest_numbers], rest_rules)
+    else
+      _buzz({s, rules}, [n | rest_numbers], rest_rules)
+    end
+  end
+
+  defp _buzz({s, rules}, [n | numbers], []) do
+    IO.puts if s == "", do: n, else: s
+    _buzz({"", rules}, numbers, rules)
+  end
+
+  defp _buzz(_, [], _) do
   end
 
 end
 
-FizzBuzz.buzz(1..100)
+FizzBuzz.buzz 1..100
